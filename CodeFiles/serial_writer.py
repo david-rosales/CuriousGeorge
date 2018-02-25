be6bb843f268c33d0ff495db57dd0d2022aa032a
David Rosales <anekin@mit.edu>
2018-02-25 00:03:24 -0500

    DASDA

--- CodeFiles/serial_writer.py
+++ CodeFiles/serial_writer.py
@@ -1,7 +1,7 @@
 import serial
 #this port need to be modified on the actual machine
-ser = serial.Serial('/dev/ttyACM0',9600)
+#ser = serial.Serial('/dev/ttyACM0',9600)
 
 def sendCommand(command):
     print("sending", command)
-    ser.write((command+"\n").encode())
+    #ser.write((command+"\n").encode())
